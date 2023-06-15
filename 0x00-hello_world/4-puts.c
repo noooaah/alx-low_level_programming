9d186e28466b5958e1d8c8f1966bb9e56392465e
noooaah <abdoelginehab435@gmail.com>
2023-06-15 12:54:48 -0700

    c-programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+  
+/**
+ * main: the main function for entry
+ * description: C program that prints exactly "Programming is like building a multilingual puzzle
+ * Return: 0 Always
+*/
+int main()
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
