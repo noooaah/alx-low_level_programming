b389752bbc6847d0ee2975661eb947052fe584fb
noooaah <abdoelginehab435@gmail.com>
2023-06-22 16:55:20 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - prints straight line
+ * description: prints a line according to a number
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+			break;
+		putchar('_');
+	}
+	_putchar('\n');
+}
+
