33981d82e6def3b46ac3bf271175e90bb5d2e5c4
noooaah <abdoelginehab435@gmail.com>
2023-06-21 19:17:10 -0700

    task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,6 +13,9 @@ void print_to_98(int n)
 
 		for (i = n; i <= 98; i++)
 		{
+			if (i == 98)
+				printf("%d\n", i);
+			else
 			printf("%d, ", i);
 		}
 	}
@@ -23,6 +26,9 @@ void print_to_98(int n)
 
 		for (j = n; j >= 98; j--)
 		{
+			if (j == 98)
+				printf("%d\n", j);
+			else
 			printf("%d, ", j);
 		}
 	}
