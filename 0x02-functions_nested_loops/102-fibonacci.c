80754b8a1677c198b3f74a726306d5786ed996ca
noooaah <abdoelginehab435@gmail.com>
2023-06-21 19:50:16 -0700

    task

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - enrty point
+ * Description: prints the first 50 Fibonacci numbers
+ * Return: 0;
+ */
+int main(void)
+{
+	int i, n;
+
+	int x = 1, y = 2, t = 50, n = x + y;
+
+	while (t--)
+	{
+		printf("%d, ", n);
+		x = y;
+		y = n;
+		n = x + y;
+	}
+
+	return (0);
+}
+
