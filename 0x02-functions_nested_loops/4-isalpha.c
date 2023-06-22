3c67bceb39e7f2e7f98276c475848928d76332a1
noooaah <abdoelginehab435@gmail.com>
2023-06-21 17:02:03 -0700

    task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - check char if lower
+ * @c: a input variable in ASCII
+ * Return: 1 if lower or upper 0 otherwise
+ * description: a program that check alphabetic
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+
+	else
+		return (0);
+
+	_putchar('\n');
+
+}
