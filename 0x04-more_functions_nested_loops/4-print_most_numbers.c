0f546ecd9419c609db3fe98af3f72a9db45f7e87
noooaah <abdoelginehab435@gmail.com>
2023-06-22 16:18:12 -0700

    more functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_most_numbers - print from 0 to 9
- * Return: 0 always
  * description: a program that print from 0 to 9
  */
 void print_most_numbers(void)
@@ -16,5 +15,4 @@ void print_most_numbers(void)
 			_putchar('0' + i);
 	}
 	_putchar('\n');
-	return (0);
 }
