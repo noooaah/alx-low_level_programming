ee0c85b7bd1f2fb6822711fb3a8a1436f43ba276
noooaah <abdoelginehab435@gmail.com>
2023-06-22 15:20:33 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - check char if upper
+ * @c: a input variable in ASCII
+ * Return: 1 if upper 0 otherwise
+ * description: a program that check uppercase
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+
+	else
+		return (0);
+
+	__putchar('\n');
+}
