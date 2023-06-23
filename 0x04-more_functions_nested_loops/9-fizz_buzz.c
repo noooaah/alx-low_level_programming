db9ac014f40fa019b634331916be6cd72778cfd7
noooaah <abdoelginehab435@gmail.com>
2023-06-22 18:40:38 -0700

    more functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,23 +2,32 @@
 #include <stdio.h>
 /**
  * main - enrty point
+ * Return: 0
  * Description: prints numbers from 0 to 100
  */
-void main(void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
+		{
 			printf("Fizz Buzz ");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz ");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
 	printf("\n");
+	return (0);
 }
-
