c17fc23fc1ec66df355ad56192e84cc1db165787
noooaah <abdoelginehab435@gmail.com>
2023-06-25 12:09:31 -0700

    update task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * description: print numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 10; i++ )
+		printf("%d", i);
+	printf('\n');
+
+	return (0);
+}
