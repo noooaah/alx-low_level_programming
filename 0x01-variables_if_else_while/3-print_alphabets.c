936d16457aeba8c0dd0113fade9986ce189786cd
noooaah <abdoelginehab435@gmail.com>
2023-06-25 12:00:04 -0700

    update task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,8 +9,12 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'Z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
 	putchar('\n');
 
 	return (0);
