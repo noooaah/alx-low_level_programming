156742c8754285ad900eec9482b07b47bbded95e
noooaah <abdoelginehab435@gmail.com>
2023-06-30 13:51:20 -0700

    more pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,15 +5,16 @@
  * @src: pointer char
  * @n: integer
  * description: copy string to another
+ * Return: string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0; i < n && src[i] != '\0' ; i++)
 		dest[i] = src[i];
-	}
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
