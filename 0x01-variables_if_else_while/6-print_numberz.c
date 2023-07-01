8b419a97e3c507c8c8d42b6b297d63f2569026fc
noooaah <abdoelginehab435@gmail.com>
2023-07-01 11:54:58 -0700

    update task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - main function
+ * description: print all single digit number
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar('0' + i);
+}
