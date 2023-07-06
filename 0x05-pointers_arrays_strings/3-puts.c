7ce0978a9696f9ad03e2eec8d0901235b8607bee
noooaah <abdoelginehab435@gmail.com>
2023-07-06 12:38:48 -0700

    pointers and array

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _puts - prints a string
+* @str: char
+* Return: string
+**/
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+}
