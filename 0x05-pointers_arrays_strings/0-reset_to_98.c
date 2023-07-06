4b1e609318e52d6a21ff78aed1f02310294c32b8
noooaah <abdoelginehab435@gmail.com>
2023-07-06 12:14:47 -0700

    pointers and array

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * reset_to_98 - make number to 98
- * @*n: integer pointer
+ * @n: integer pointer
  * description: change any number to 98
- * Return: 98
  */
 void reset_to_98(int *n)
 {
