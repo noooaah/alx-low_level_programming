9d51b84609777f856d0ddfd2e81004406632e16d
noooaah <abdoelginehab435@gmail.com>
2023-07-17 19:22:52 -0700

    Structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - dog data
+* @name: name of dog
+* @age: age of dog
+* @owner: name of owner dogs
+* Description: Defines metadata about the dog and type of dogs
+**/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
