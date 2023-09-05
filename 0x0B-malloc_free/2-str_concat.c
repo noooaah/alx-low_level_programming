a5bf0c3bebf4cb855bd057797d34d1d3f4673246
noooaah <abdoelginehab435@gmail.com>
2023-09-05 16:38:24 -0700

    malloc 0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,15 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k = 0;
 	char *p;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
