395a11853c42106bb15ec574929c94f138e9705e
noooaah <abdoelginehab435@gmail.com>
2023-09-06 17:57:52 -0700

    Debugging

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - entry point
+* @argc: size of argv
+* @argv: array
+* Return: 0
+**/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
