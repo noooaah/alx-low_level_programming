be1eabda4ce2022fe0b1e5ea6e95ac8f5da4458b
noooaah <abdoelginehab435@gmail.com>
2023-09-11 18:22:57 -0700

    struct,typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,15 +7,9 @@
 * @owner: name of owner dogs
 * Description: Defines metadata about the dog and type of dogs
 **/
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif
+};
