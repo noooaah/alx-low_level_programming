2817cebaf7dbe960b0fd16fba5df9f99904660b1
noooaah <abdoelginehab435@gmail.com>
2023-09-28 13:28:18 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,17 +7,14 @@
 * @b: pointing to a string of 0 and 1 chars
 * Return: the converted number or 0
 **/
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, sum = 0;
-	
 
 	if (b == NULL)
 		return (0);
 	while (b[i] != '\0')
 	{
-		
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
