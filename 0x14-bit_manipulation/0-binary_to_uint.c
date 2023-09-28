c175b12460094af0cc4055b36337f0e1b8403f42
noooaah <abdoelginehab435@gmail.com>
2023-09-28 13:22:04 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,14 +12,16 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, sum = 0;
 	
+
 	if (b == NULL)
 		return (0);
-	while (b != '\0')
+	while (b[i] != '\0')
 	{
+		
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
-		sum += b[i] - '0';
+		sum = (sum << 1) + (b[i] - '0');
 		i++;
 	}
 	return (sum);
